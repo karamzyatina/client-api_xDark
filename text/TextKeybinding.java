7ca2c668630db0d4d99b39f5d8075887b080604e
karamzyatina <67735224+karamzyatina@users.noreply.github.com>
2022-01-05 09:14:04 +0200

    ne to

--- text/TextKeybinding.java
+++
@@ -1,6 +0,0 @@
-package dev.xdark.clientapi.text;
-
-public interface TextKeybinding extends Text
-{
-    String getKeybinding();
-}
--- text/TextLiteral.java
+++
@@ -1,6 +0,0 @@
-package dev.xdark.clientapi.text;
-
-public interface TextLiteral extends Text
-{
-    String getLiteral();
-}
